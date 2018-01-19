12c1498f86546554635c628b195bf8df8bb1425f
lfrydrych <lfrydrych@pgs-soft.com>
2018-01-19 10:48:37 +0100

    tests

--- testwarez/src/androidTest/java/com/pgssoft/testwarez/test/test/CucumberTestCase.kt
+++ testwarez/src/androidTest/java/com/pgssoft/testwarez/test/test/CucumberTestCase.kt
@@ -4,9 +4,10 @@ import cucumber.api.CucumberOptions
 
 @CucumberOptions(features = arrayOf("features"),
         glue = arrayOf("com.pgssoft.testwarez.test.cucumber.steps"),
-        plugin = arrayOf("pretty",
-                "html:/mnt/sdcard/cucumber-reports/cucumber-html-report",
-                "json:/mnt/sdcard/cucumber-reports/cucumber.json",
-                "junit:/mnt/sdcard/cucumber-reports/cucumber.xml"),
+        plugin = arrayOf("pretty"
+//                "html:/mnt/sdcard/cucumber-reports/cucumber-html-report",
+//                "json:/mnt/sdcard/cucumber-reports/cucumber.json",
+//                "junit:/mnt/sdcard/cucumber-reports/cucumber.xml"
+        ),
         tags = arrayOf("@agenda, @test1, @test2"))
 class CucumberTestCase
